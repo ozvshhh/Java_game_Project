3bead9038cccd6441c14b002ef8e7c0953f954e2
ozvshhh <dlgkdms4660@gmail.com>
2023-07-27 21:44:46 +0900

    class 생성

---
+++ src/Progress.java
@@ -0,0 +1,23 @@
+public class Progress {
+
+}
+
+public class Unit{
+    public class Enemy extends Unit{
+
+    }
+
+    public class Player extends Unit{
+        public class Tpe extends Player{}
+        public class GP extends Player{}
+        public class Shaco extends Player{}
+    }
+
+}
+
+public class Game{
+    public class Card extends Game{}
+    public class Gun extends Game{}
+    public class Box extends Game{}
+
+}
